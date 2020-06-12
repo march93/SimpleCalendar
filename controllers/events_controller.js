893f44c64a19aec6639af2f9db388fbdd9c13a47
march93 <mho@tractionondemand.com>
2020-06-11 21:02:07 -0700

    Setup user and event endpoint logic

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -1,7 +1,24 @@
-exports.get_event = (req, res) => {
-    res.send({ id: 1 });
+const Event = require('../models/event');
+
+exports.get_event = async (req, res) => {
+    const event = await Event.findAll({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    res.json(event);
 }
 
-exports.create_event = (req, res) => {
-    res.send();
+exports.create_event = async (req, res) => {
+    const body = req.body;
+
+    const event = await Event.create({
+        title: body.title,
+        startTime: body.startTime,
+        endTime: body.endTime,
+        UserId: req.params.id
+    });
+
+    res.json(event);
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,11 @@
-exports.get_user = (req, res) => {
-   res.send();
+const User = require('../models/user');
+
+exports.get_user = async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    return res.json(user);
 }
